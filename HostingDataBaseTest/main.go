2f5b7ff794f2e6dbdbcddc76d9452c13105ec670
Boora sri Harsha Vardhan <138381780+harshavardhanyadav2004@users.noreply.github.com>
2024-10-12 13:46:21 +0530

    Add files via upload

---
+++ HostingDataBaseTest/main.go
@@ -0,0 +1,11 @@
+package main 
+import (
+	"fmt"
+	"HostingDataBaseTest/hosting"
+)
+
+func main(){
+	 fmt.Println("Inserting into the database")
+	// hosting.PushIntoTheHost()
+	hosting.RetreiveFromTheDataBase()
+}
